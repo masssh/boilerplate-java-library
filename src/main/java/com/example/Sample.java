814dbd9fe4a55888d0bb6f1aa4e7cf8863e48ba1
masssh <utm.ver.2.0@gmail.com>
2019-10-20 00:02:46 +0900

    Add sample test

---
+++ src/main/java/com/example/Sample.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Sample {
+    public int foo() {
+        return 1;
+    }
+}
---
+++ src/test/java/com/example/SampleTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SampleTest {
+    @Test
+    void shouldTest() {
+        final Sample sample = new Sample();
+        final int result = sample.foo();
+        assertEquals(result, 1);
+    }
+}
