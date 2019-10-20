a2c30ddd4191f25be3825465e919e1b40b0ad5ae
masssh <utm.ver.2.0@gmail.com>
2019-10-20 11:51:27 +0900

    Rename package

--- src/main/java/masssh/boilerplate/library/Sample.java
+++ src/main/java/masssh/boilerplate/library/Sample.java
@@ -1,4 +1,4 @@
-package com.example;
+package masssh.boilerplate.library;
 
 public class Sample {
     public int foo() {
--- src/test/java/masssh/boilerplate/library/SampleTest.java
+++ src/test/java/masssh/boilerplate/library/SampleTest.java
@@ -1,4 +1,4 @@
-package com.example;
+package masssh.boilerplate.library;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
